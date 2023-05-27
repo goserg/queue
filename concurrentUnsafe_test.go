f8281ffebd77b5c8dcc09c1aa7721b5ce967f6cf
goserg <gor-serg@bk.ru>
2023-05-27 23:27:27 +0300

    add test for scale after queue rotation; refactor tests

--- concurrentUnsafe_test.go
+++ concurrentUnsafe_test.go
@@ -7,13 +7,7 @@ import (
 func TestZeroConcurrentUnsafeQueue_Push(t *testing.T) {
 	o := NewConcurrentUnsafe[int](0)
 	o.Push(777)
-	v, err := o.Pop()
-	if err != nil {
-		t.Fatalf("expect nil err, got %v", err)
-	}
-	if v != 777 {
-		t.Fatalf("expect %d got %d", 777, v)
-	}
+	popWithCheck(t, o, 777)
 }
 
 func TestConcurrentUnsafeQueue_Push(t *testing.T) {
@@ -21,13 +15,7 @@ func TestConcurrentUnsafeQueue_Push(t *testing.T) {
 	q := NewConcurrentUnsafe[int](3)
 	for _, val := range testData {
 		q.Push(val)
-		v, err := q.Pop()
-		if err != nil {
-			t.Fatalf("expect nil err, got %v", err)
-		}
-		if v != val {
-			t.Fatalf("expect %d got %d", val, v)
-		}
+		popWithCheck(t, q, val)
 	}
 }
 
@@ -38,12 +26,42 @@ func TestConcurrentUnsafeQueue_ScaleUp(t *testing.T) {
 		q.Push(val)
 	}
 	for _, val := range testData {
-		v, err := q.Pop()
-		if err != nil {
-			t.Fatalf("expect nil err, got %v", err)
-		}
-		if v != val {
-			t.Fatalf("expect %d got %d", val, v)
-		}
+		popWithCheck(t, q, val)
+	}
+}
+
+func TestConcurrentUnsafeQueue_ScaleUpWithRotation(t *testing.T) {
+	testBatch1 := []int{12, 34, 685}
+	testBatch2 := []int{5, 2, 5}
+	testBatch3 := []int{5, 65, 123, 63, 56}
+	q := NewConcurrentUnsafe[int](7)
+	for _, i := range testBatch1 {
+		q.Push(i)
+	}
+	for _, i := range testBatch2 {
+		q.Push(i)
+	}
+	for _, i := range testBatch1 {
+		popWithCheck(t, q, i)
+	}
+	for _, i := range testBatch3 {
+		q.Push(i)
+	}
+	for _, i := range testBatch2 {
+		popWithCheck(t, q, i)
+	}
+	for _, i := range testBatch3 {
+		popWithCheck(t, q, i)
+	}
+}
+
+func popWithCheck[T comparable](t *testing.T, q *ConcurrentUnsafeQueue[T], expected T) {
+	t.Helper()
+	v, err := q.Pop()
+	if err != nil {
+		t.Fatalf("expect nil err, got %v", err)
+	}
+	if v != expected {
+		t.Fatalf("expect %v got %v", expected, v)
 	}
 }
--- queue_test.go
+++ queue_test.go
@@ -6,50 +6,6 @@ import (
 	"testing"
 )
 
-func TestZeroQueue_Push(t *testing.T) {
-	o := New[int](0)
-	o.Push(777)
-	v, err := o.Pop()
-	if err != nil {
-		t.Fatalf("expect nil err, got %v", err)
-	}
-	if v != 777 {
-		t.Fatalf("expect %d got %d", 777, v)
-	}
-}
-
-func TestQueue_Push(t *testing.T) {
-	testData := []int{12, 34, 5, 2, 5, 65, 123, 65, 3, 4, 32, 54, 12, 76, 78, 3, 2, 15}
-	q := New[int](3)
-	for _, val := range testData {
-		q.Push(val)
-		v, err := q.Pop()
-		if err != nil {
-			t.Fatalf("expect nil err, got %v", err)
-		}
-		if v != val {
-			t.Fatalf("expect %d got %d", val, v)
-		}
-	}
-}
-
-func TestQueue_ScaleUp(t *testing.T) {
-	testData := []int{12, 34, 5, 2, 5, 65, 123, 65, 3, 4, 32, 54, 12, 76, 78, 3, 2, 15}
-	q := New[int](3)
-	for _, val := range testData {
-		q.Push(val)
-	}
-	for _, val := range testData {
-		v, err := q.Pop()
-		if err != nil {
-			t.Fatalf("expect nil err, got %v", err)
-		}
-		if v != val {
-			t.Fatalf("expect %d got %d", val, v)
-		}
-	}
-}
-
 func TestConcurrent(t *testing.T) {
 	testData := map[int]struct{}{
 		2:   {},
